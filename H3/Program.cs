7d68427412fa70ba7bdc0b6d806da364e4329736
Sadıkcan TULUK <sadikcantuluk@gmail.com>
2022-10-23 21:21:39 +0300

    H3 UYGULAMA

---
+++ H3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H3_UYG
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Student student = new Student(0, "ÇINAR", "BEYAZGÜL");
+
+            List<Student> students = new List<Student>()
+            {
+                new Student(1, "AYŞEGÜL", "TULUK"),
+                new Student(2, "SADIK", "TULUK"),
+                new Student(3, "EDANUR", "TULUK"),
+            };
+            StudentManager studentManager = new StudentManager(students);
+            studentManager.ReadList();
+            studentManager.CreateStudent(student);
+            studentManager.DeleteStudent(2);
+            studentManager.UpdateStudent(1, "MEHTAP", "TULUK");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ H3/StudentManager.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H3_UYG
+{
+    public class StudentManager
+    {
+        List<Student> students;
+
+        public StudentManager()
+        {
+            students = new List<Student>();
+        }
+
+        public StudentManager(List<Student> std)
+        {
+            students = std;
+        }
+
+        public StudentManager(Student std)
+        {
+            students = new List<Student>();
+            CreateStudent(std);
+        }
+
+        // Create
+        public void CreateStudent(Student student)
+        {
+            students.Add(student);
+        }
+
+        // Read
+        public Student ReadStudent(int id)
+        {
+            return students[id];
+        }
+
+        // Update
+        public void UpdateStudent(int id, string firstName, string lastName)
+        {
+            if (firstName != null)
+                students[id].FirstName = firstName;
+            if (lastName != null)
+                students[id].LastName = lastName;
+        }
+
+        // Delete
+        public Student DeleteStudent(int id)
+        {
+            var std = ReadStudent(id);
+            // stdList.Remove(std);
+            students.RemoveAt(id);
+            return std;
+        }
+
+        public void ReadList()
+        {
+            foreach (var item in students)
+                Console.WriteLine(item);
+        }
+    }
+}
